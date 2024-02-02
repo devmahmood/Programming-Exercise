c34037222e36d9077baf643529c66c5afb3ed773
Abdullah Al Mahmud <aamahmud.cs@gmail.com>
2024-02-02 16:04:14 +0600

    Basic CPP Programs

---
+++ 2-basic-print-name.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string name;
+    cout<<"Enter Your Name: ";
+    getline(cin, name);
+
+    cout<<"Entered Name is : "<<name;
+
+    return 0;
+}
---
+++ 3-basic-input.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i;
+
+    //take input
+    cin >> i;
+
+    //print output
+    cout << i;
+
+    return 0;
+}
